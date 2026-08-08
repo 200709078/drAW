9f8b11c952e1a6887a003ecaf9a019c5280670b0
200709078 <ademvarol@posta.mu.edu.tr>
2026-08-08 08:27:05 +0300

    AFTER ANDROID

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.madematik.draw',
+  appName: 'drAW',
+  webDir: 'dist'
+};
+
+export default config;
