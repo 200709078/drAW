705b7a950a9a17edbec3d1069c0e75a45404f1b1
200709078 <ademvarol@posta.mu.edu.tr>
2026-09-25 00:44:18 +0300

    PHOTO LINK FIXES: NAV BUTTONS, STATUS, UNLINK SYNC

--- src/tools/SelectionTool.ts
+++ src/tools/SelectionTool.ts
@@ -1212,13 +1212,14 @@ export class SelectionTool extends Tool {
             );
         }
 
+        const midY = (bounds.minY + bounds.maxY) / 2;
         this.prevPhotoButton.style.left = `${viewport.worldToScreenX(bounds.minX + 40)}px`;
-        this.prevPhotoButton.style.top = `${viewport.worldToScreenY((bounds.minY + bounds.maxY) / 2) + getTitlebarOffset()}px`;
+        this.prevPhotoButton.style.top = `${viewport.worldToScreenY(midY - 26) + getTitlebarOffset()}px`;
         this.prevPhotoButton.hidden = false;
         this.prevPhotoButton.disabled = this.photoNavigator?.canStepPhoto(-1) !== true;
 
-        this.nextPhotoButton.style.left = `${viewport.worldToScreenX(bounds.maxX - 40)}px`;
-        this.nextPhotoButton.style.top = `${viewport.worldToScreenY((bounds.minY + bounds.maxY) / 2) + getTitlebarOffset()}px`;
+        this.nextPhotoButton.style.left = `${viewport.worldToScreenX(bounds.minX + 40)}px`;
+        this.nextPhotoButton.style.top = `${viewport.worldToScreenY(midY + 26) + getTitlebarOffset()}px`;
         this.nextPhotoButton.hidden = false;
         this.nextPhotoButton.disabled = this.photoNavigator?.canStepPhoto(1) !== true;
 
